fix attack error by build bin inside computation graph

alg/lowerbound_functions.py
@@ -292,7 +292,7 @@ def encoding(enc_mlp, fea, y, K, use_mean=False, fix_samples=False, seed=0):
 #     return bound
 
 def lowerbound_F(x, fea, y, enc_mlp, dec, ll, K=1, IS=False, 
-               use_mean=False, fix_samples=False, seed=0, z=None, beta=1.0, categorical=False, x_cat=None):
+               use_mean=False, fix_samples=False, seed=0, z=None, beta=1.0, categorical=False):
     if use_mean:
         K = 1
         fix_samples=False
@@ -318,7 +318,9 @@ def lowerbound_F(x, fea, y, enc_mlp, dec, ll, K=1, IS=False,
     y_rep = y
 
     if categorical:
-        x_rep = x_cat
+        # x_rep = x_cat
+        bin_num=128
+        x_rep = tf.dtypes.cast(x_rep * (bin_num-1), tf.int32)
 
     # prior
     log_prior_z = log_gaussian_prob(z, 0.0, 0.0)

alg/vae_new.py
@@ -10,7 +10,7 @@ def logsumexp(x):
     tmp = tf.log(tf.clip_by_value(tf.reduce_sum(tf.exp(x_), 0), 1e-9, np.inf))
     return tmp + x_max
 
-def bayes_classifier(x, enc, dec, ll, dimY, lowerbound, K = 1, beta=1.0, categorical=False, x_cat=None):
+def bayes_classifier(x, enc, dec, ll, dimY, lowerbound, K = 1, beta=1.0, categorical=False):
     enc_conv, enc_mlp = enc
     fea = enc_conv(x)
     N = x.get_shape().as_list()[0]
@@ -24,7 +24,7 @@ def bayes_classifier(x, enc, dec, ll, dimY, lowerbound, K = 1, beta=1.0, categor
         bound_sum=0
         mctimes=50
         for jj in range(mctimes):
-            bound, debug_list = lowerbound(x, fea, y, enc_mlp, dec, ll, K, IS=True, beta=beta, categorical=categorical, x_cat=x_cat)
+            bound, debug_list = lowerbound(x, fea, y, enc_mlp, dec, ll, K, IS=True, beta=beta, categorical=categorical)
             bound_sum += bound
 
         logpxy.append(tf.expand_dims(bound_sum/mctimes, 1))
@@ -42,7 +42,7 @@ def categorize(X, bin_num):
     X = X * (bin_num-1)
     return X.astype(int)
 
-def construct_optimizer(X_ph, Y_ph, enc, dec, ll, K, vae_type='A', categorical=False, x_cat=None):
+def construct_optimizer(X_ph, Y_ph, enc, dec, ll, K, vae_type='A', categorical=False):
 
     # loss function
     enc_conv, enc_mlp = enc
@@ -73,13 +73,13 @@ def construct_optimizer(X_ph, Y_ph, enc, dec, ll, K, vae_type='A', categorical=F
     #     from lowerbound_functions import lowerbound_G as lowerbound_func
 
     beta_ph = tf.placeholder(tf.float32, shape=(), name='beta')
-    bound, debug_list = lowerbound_func(X_, fea, Y_ph, enc_mlp, dec, ll_, K, IS=True, beta=beta_ph, categorical = categorical, x_cat=x_cat)
+    bound, debug_list = lowerbound_func(X_, fea, Y_ph, enc_mlp, dec, ll_, K, IS=True, beta=beta_ph, categorical = categorical)
     bound = tf.reduce_mean(bound)
     batch_size = X_ph.get_shape().as_list()[0]
 
     # also evaluate approx Bayes classifier's accuracy
     dimY = Y_ph.get_shape().as_list()[-1]
-    y_pred = bayes_classifier(X_, enc, dec, ll_, dimY, lowerbound_func, K=10, beta=beta_ph, categorical=categorical, x_cat=x_cat) # TODO: weired
+    y_pred = bayes_classifier(X_, enc, dec, ll_, dimY, lowerbound_func, K=10, beta=beta_ph, categorical=categorical) # TODO: weired
     correct_prediction = tf.equal(tf.argmax(Y_ph,1), tf.argmax(y_pred,1))
     acc_train = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -92,7 +92,7 @@ def construct_optimizer(X_ph, Y_ph, enc, dec, ll, K, vae_type='A', categorical=F
         return cost, logpx_z, logpy_z, negKL_value
 
     def train_cat(sess, X, X_catogrical, Y, lr, beta):
-        _, cost, logpx_z, logpy_z, negKL_value = sess.run(ops, feed_dict={X_ph: X, x_cat:X_catogrical, Y_ph: Y, lr_ph: lr, beta_ph: beta})
+        _, cost, logpx_z, logpy_z, negKL_value = sess.run(ops, feed_dict={X_ph: X, Y_ph: Y, lr_ph: lr, beta_ph: beta})
         return cost, logpx_z, logpy_z, negKL_value
 
     def fit(sess, X, Y, n_iter, lr, beta):
@@ -158,7 +158,7 @@ def construct_optimizer(X_ph, Y_ph, enc, dec, ll, K, vae_type='A', categorical=F
             begin = end
 
     def eval_categorical(sess, X, Y, data_name = 'train', beta=1.0):
-        X_categorical = categorize(np.copy(X), bin_num=128)
+        # X_categorical = categorize(np.copy(X), bin_num=128)
         N = X.shape[0]
         begin = time.time()
         n_batch = int(N / batch_size)
@@ -169,7 +169,7 @@ def construct_optimizer(X_ph, Y_ph, enc, dec, ll, K, vae_type='A', categorical=F
             indr = min((j+1) * batch_size, N)
             res1, res2 = sess.run((acc_train, bound), feed_dict={X_ph:X[indl:indr],
                                                                  Y_ph: Y[indl:indr],
-                                                                 x_cat: X_categorical[indl:indr],
+                                                                 # x_cat: X_categorical[indl:indr],
                                                                  beta_ph: beta})
             acc_total += res1 / n_batch
             bound_total += res2 / n_batch

test_attacks/attack.py
@@ -64,13 +64,10 @@ def test_attacks(data_name, model_name, attack_method, eps, batch_size=100,
     print('use batch_size = %d' % batch_size)
     x = tf.placeholder(tf.float32, shape=(batch_size, 25))
     y = tf.placeholder(tf.float32, shape=(batch_size, 2))
-    if categorical:
-        X_cat = tf.placeholder(tf.int32, shape=(batch_size, 25))
-    else:
-        X_cat = None
+
 
     # Define TF model graph  
-    model = load_classifier(sess, model_name, data_name, categorical=categorical, x_cat=X_cat, dimZ=args.dimZ)
+    model = load_classifier(sess, model_name, data_name, categorical=categorical, dimZ=args.dimZ)
     if 'bayes' in model_name and 'distill' not in model_name and 'query' not in model_name:
         model_name = model_name + '_cnn'
 

test_attacks/load/load_bayes_classifier.py
@@ -9,7 +9,7 @@ sys.path.extend([PATH+'alg/', PATH+'models/', PATH+'utils/'])
 class BayesModel:
     def __init__(self, sess, data_name, vae_type, conv=True, K=1, checkpoint=0, 
                  attack_snapshot=False, use_mean=False, fix_samples=False, no_z=False,
-                 dimZ=None, categorical=False, x_cat=None):
+                 dimZ=None, categorical=False):
         if data_name == 'mnist':
             self.num_channels = 1
             self.image_size = 28
@@ -45,7 +45,7 @@ class BayesModel:
         cla, test_ll, enc, dec = load_bayes_classifier(sess, data_name, vae_type, K, checkpoint, 
                                                        conv=conv, attack_snapshot=attack_snapshot, 
                                                        use_mean=use_mean, fix_samples=fix_samples, 
-                                                       no_z=no_z, dimZ=dimZ, categorical=categorical, x_cat=x_cat)
+                                                       no_z=no_z, dimZ=dimZ, categorical=categorical)
         self.model = cla
         self.eval_test_ll = test_ll
         self.enc = enc
@@ -91,7 +91,7 @@ def logsumexp(x):
 
 def bayes_classifier(x, enc, dec, ll, dimY, dimZ, lowerbound, K = 1, beta=1.0, use_mean=False,
                      fix_samples=False, snapshot=False, seed=0, no_z=False, softmax=False,
-                     categorical=False, x_cat=None):
+                     categorical=False):
     if use_mean: K=1
     enc_conv, enc_mlp = enc
     fea = enc_conv(x)
@@ -105,7 +105,7 @@ def bayes_classifier(x, enc, dec, ll, dimY, dimZ, lowerbound, K = 1, beta=1.0, u
     for i in range(dimY):
         y = np.zeros([N, dimY]); y[:, i] = 1; y = tf.constant(np.asarray(y, dtype='f'))
         bound, debug_list = lowerbound(x, fea, y, enc_mlp, dec, ll, K, IS=False, beta=beta,
-                           use_mean=use_mean, fix_samples=fix_samples, seed=seed, z=z_holder, categorical=categorical, x_cat=x_cat)
+                           use_mean=use_mean, fix_samples=fix_samples, seed=seed, z=z_holder, categorical=categorical)
         print('bound shape', bound.shape)
         logpxy.append(tf.expand_dims(bound, 1))
     logpxy = tf.concat(logpxy, 1)
@@ -122,7 +122,7 @@ def bayes_classifier(x, enc, dec, ll, dimY, dimZ, lowerbound, K = 1, beta=1.0, u
 
 def load_bayes_classifier(sess, data_name, vae_type, K, checkpoint=0, conv=True, 
                           attack_snapshot=False, use_mean=False, fix_samples=False, no_z=False,
-                          dimZ=None, categorical=False, bin_num=128, x_cat=None):
+                          dimZ=None, categorical=False, bin_num=128):
     if data_name == 'mnist':
         input_shape = (28, 28, 1)
         dimX = 28**2
@@ -215,23 +215,23 @@ def load_bayes_classifier(sess, data_name, vae_type, K, checkpoint=0, conv=True,
 
     def comp_test_ll(x, y, K):
         fea = identity(x)
-        bound = lowerbound(x, fea, y, enc, dec, ll, K, IS=True, beta=beta, use_mean=use_mean, categorical=categorical, x_cat=x_cat)
+        bound = lowerbound(x, fea, y, enc, dec, ll, K, IS=True, beta=beta, use_mean=use_mean, categorical=categorical)
         return tf.reduce_mean(bound)
     
     def classifier(x):
-        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K, beta, categorical=categorical, x_cat=x_cat)
+        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K, beta, categorical=categorical)
         
     def classifier_snapshot(x):
-        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K, beta, snapshot=True, categorical=categorical, x_cat=x_cat)
+        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K, beta, snapshot=True, categorical=categorical)
     
     def classifier_use_mean(x):
-        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, 1, beta, use_mean=True, categorical=categorical, x_cat=x_cat)
+        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, 1, beta, use_mean=True, categorical=categorical)
     
     def classifier_fix_samples(x):
-        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K, beta, fix_samples=True, categorical=categorical, x_cat=x_cat)
+        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K, beta, fix_samples=True, categorical=categorical)
 
     def classifier_no_z(x):
-        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K=1, beta=beta, no_z=True, categorical=categorical, x_cat=x_cat)
+        return bayes_classifier(x, [enc_conv, enc_mlp], dec, ll, dimY, dimZ, bound_func, K=1, beta=beta, no_z=True, categorical=categorical)
 
     if attack_snapshot:
         print("use %d samples, and attack each of them" % K)

vae_spam.py
@@ -73,10 +73,10 @@ def main(data_name, vae_type, dimZ, dimH, n_iter, batch_size, K, checkpoint, cat
     # define optimisers
     X_ph = tf.placeholder(tf.float32, shape=(batch_size,)+input_shape)  #TODO: ??
     Y_ph = tf.placeholder(tf.float32, shape=(batch_size, dimY))
-    if categorical:
-        X_cat = tf.placeholder(tf.int32, shape=(batch_size, 25))
-    else:
-        X_cat = None
+    # if categorical:
+    #     X_cat = tf.placeholder(tf.int32, shape=(batch_size, 25))
+    # else:
+    # X_cat = None
 
     print('X_ph.shape:', X_ph.shape)
     print('Y_ph.shape:', Y_ph.shape)
@@ -86,7 +86,7 @@ def main(data_name, vae_type, dimZ, dimH, n_iter, batch_size, K, checkpoint, cat
     #ll = 'l2'
     ll = 'xe'
     identity = lambda x: x
-    fit, eval_acc = construct_optimizer(X_ph, Y_ph, [identity, enc], dec, ll, K, vae_type, categorical, X_cat)
+    fit, eval_acc = construct_optimizer(X_ph, Y_ph, [identity, enc], dec, ll, K, vae_type, categorical)
 
     # initialise sessions
     config = tf.ConfigProto()