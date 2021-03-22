b9d9e7a3708d1faf7aa918756619d99b0e71ae3e
Yizheng Chen <surrealyz@gmail.com>
2021-03-22 17:33:03 -0400

    mlp_generator_categorical_pdf.py

---
+++ models/mlp_generator_categorical_pdf.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+from mlp import mlp_layer
+
+"""
+generator p(z)p(x|z)
+"""
+
+
+def generator(dimX, dimH, dimZ, last_activation, name, bin_num=2):
+    # construct p(x|z)
+    fc_layers = [dimZ, dimH, dimH, dimX*bin_num]
+    l = 0
+    pxz_mlp_layers = []
+    N_layers = len(fc_layers) - 1
+    for i in range(N_layers):
+        if i < N_layers - 1:
+            activation = 'relu'
+        else:
+            activation = last_activation
+        name_layer = name + '_pxz_mlp_l%d' % l
+        with tf.variable_scope('vae'):
+            pxz_mlp_layers.append(mlp_layer(fc_layers[i], fc_layers[i + 1], activation, name_layer))
+        l += 1
+
+    def pxz_params(z):
+        out = z
+        for layer in pxz_mlp_layers:
+            out = layer(out)
+        out = tf.reshape(out, [-1, 3514, bin_num])
+        return out
+
+    return pxz_params
